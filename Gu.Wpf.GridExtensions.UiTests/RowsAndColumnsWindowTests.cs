285be24300bf9609509a369a7fcbebf016ecf458
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-07 18:31:42 +0200

    Use CultureInfo.InvariantCulture in bounds tests.

--- Gu.Wpf.GridExtensions.UiTests/RowsAndColumnsWindowTests.cs
+++ Gu.Wpf.GridExtensions.UiTests/RowsAndColumnsWindowTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Gu.Wpf.GridExtensions.UiTests
 {
+    using System.Globalization;
     using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
@@ -23,19 +24,19 @@
             Application.KillLaunched(Exe);
         }
 
-        [TestCase("0 0", "7;7;200;100")]
-        [TestCase("0 1", "207;7;100;100")]
-        [TestCase("1 0", "7;107;200;100")]
-        [TestCase("1 1", "207;107;100;100")]
-        [TestCase("2 0", "7;207;200;50")]
-        [TestCase("2 1", "207;207;100;50")]
+        [TestCase("0 0", "7,7,200,100")]
+        [TestCase("0 1", "207,7,100,100")]
+        [TestCase("1 0", "7,107,200,100")]
+        [TestCase("1 1", "207,107,100,100")]
+        [TestCase("2 0", "7,207,200,50")]
+        [TestCase("2 1", "207,207,100,50")]
         public void Bounds(string header, string bounds)
         {
             using (var app = Application.AttachOrLaunch(Exe, WindowName))
             {
                 var window = app.MainWindow;
                 var box = window.FindGroupBox(header);
-                Assert.AreEqual(bounds, box.Bounds.ToString());
+                Assert.AreEqual(bounds, box.Bounds.ToString(CultureInfo.InvariantCulture));
             }
         }
     }
