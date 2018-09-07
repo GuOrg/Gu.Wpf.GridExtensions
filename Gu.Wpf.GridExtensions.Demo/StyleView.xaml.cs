5565bafe151991269b1ac191db71942c02e9fe72
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-07 16:24:41 +0200

    UI-test clicking all tabs. #4.

--- Gu.Wpf.GridExtensions.Demo/StyleView.xaml.cs
+++ Gu.Wpf.GridExtensions.Demo/StyleView.xaml.cs
@@ -2,9 +2,6 @@
 {
     using System.Windows.Controls;
 
-    /// <summary>
-    /// Interaction logic for StyleView.xaml.
-    /// </summary>
     public partial class StyleView : UserControl
     {
         public StyleView()
---
+++ Gu.Wpf.GridExtensions.UiTests/MainWindowTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Gu.Wpf.GridExtensions.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class MainWindowTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so we don't crash.
+            using (var app = Application.Launch("Gu.Wpf.GridExtensions.Demo.exe"))
+            {
+                var window = app.MainWindow;
+                var tab = window.FindTabControl();
+                foreach (var tabItem in tab.Items)
+                {
+                    _ = tabItem.Select();
+                }
+            }
+        }
+    }
+}
