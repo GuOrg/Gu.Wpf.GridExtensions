6b9e6338cb4da622266095d8ad1f0ba331af6929
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-28 18:12:10 +0100

    New csproj and nuget

---
+++ Gu.Wpf.GridExtensions/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf", "Gu.Wpf.GridExtensions")]
+[assembly: XmlnsPrefix("https://github.com/JohanLarsson/Gu.Wpf", "gu")]
